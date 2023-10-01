a3affa4a6e499080b622ca533ec761e7acf22e26
The_Deerman <113066400+The-Deerman@users.noreply.github.com>
2023-10-01 21:17:38 +0200

    Actually add wizard warp
    Thanks SpiringJurre

--- SoopyV2/features/events/index.js
+++ SoopyV2/features/events/index.js
@@ -17,7 +17,7 @@ let warpData={
 "castle":[-250,130,45],
 "da":[91,75,176],
 "museum":[-75,76,80],
-"wizard":[42,90,67],
+"wizard_tower":[42,122,69],
 "hub":[-2,70,-69]};
 
 function getKeyBindFromKey(key,description){

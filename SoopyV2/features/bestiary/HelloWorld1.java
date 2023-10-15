a4c685e6d15b64b87021c9a62c2096f8c93bac44
The_Deerman <113066400+The-Deerman@users.noreply.github.com>
2023-10-15 16:21:33 +0200

    Removed HelloWorld1 java file

--- SoopyV2/features/bestiary/HelloWorld1.java
+++
@@ -1,9 +0,0 @@
-import net.minecraft.client.Minecraft;
-import net.minecraft.util.ChatComponentText;
-
-class HelloWorld1 {
-    public static void helloWorld() {
-        ChatComponentText cp = new ChatComponentText("WICKED GAMINJG RIGHT HERE!!!");
-        Minecraft.getMinecraft().thePlayer.addChatMessage(cp);
-    }
-}
